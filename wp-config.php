f8f064fff997f57be4e0df0f03aa04aa48833946
shashank <shashank.u@lucideustech.com>
2018-05-01 10:08:26 +0530

    plugin

--- wp-config.php
+++ wp-config.php
@@ -20,16 +20,16 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define('DB_NAME', 'yoga-new');
+define('DB_NAME', 'yogadevvrat');
 
 /** MySQL database username */
-define('DB_USER', 'root');
+define('DB_USER', 'yoga');
 
 /** MySQL database password */
-define('DB_PASSWORD', '');
+define('DB_PASSWORD', 'yoga');
 
 /** MySQL hostname */
-define('DB_HOST', 'localhost');
+define('DB_HOST', '13.229.174.44');
 
 /** Database Charset to use in creating database tables. */
 define('DB_CHARSET', 'utf8');
