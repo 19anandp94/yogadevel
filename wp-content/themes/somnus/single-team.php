5942e0a9554ea1ed94984c91c33fe2cc225bcb07
safe <krishna.s@lucideustech.com>
2018-09-15 17:03:20 +0530

    hide backgroun images

--- wp-content/themes/somnus/single-team.php
+++ wp-content/themes/somnus/single-team.php
@@ -5,13 +5,13 @@
 	$sidebar = ( is_active_sidebar('primary') ) ? 'post-sidebar' : 'post';
 ?>
 
-<?php if( has_post_thumbnail() ) : ?>        
+<!-- <?php if( has_post_thumbnail() ) : ?>        
 	<section class="pt240 pb160 pt-xs-80 pb-xs-80 image-bg page-title">
 		<div class="background-image-holder">
 			<?php the_post_thumbnail('full', array('class' => 'background-image')); ?>
 		</div>
 	</section>
-<?php endif; ?>
+<?php endif; ?> -->
 
 <section id="post-<?php the_ID(); ?>" <?php post_class('contained-page p0'); ?>>
 	<div class="container">
